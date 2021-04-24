bea6d91a20e01c700834e52e4e245e867e4b2e8d
Jarrod Pas <jarrod@jarrodpas.com>
2021-04-23 18:33:16 -0600

    benchmark: add benchmark log to csv converter

---
+++ scripts/benchmark-to-csv.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+import csv
+
+from collections import defaultdict
+from io import StringIO
+
+def parse_line(line):
+    path, result = line.strip().split(': ')
+
+    scheme, degree, *measure = path.split('/')
+
+    value, unit = result.split(' ')
+    value = int(value)
+
+    return (scheme, degree), tuple(measure), (value, unit)
+
+def main():
+    rows = defaultdict(dict)
+    fields = set()
+
+    for params, measure, (result, unit) in map(parse_line, sys.stdin):
+        if measure[-1] == 'ops':
+            continue
+
+        scheme, degree = params
+        rows[params]['scheme'] = scheme
+        rows[params]['degree'] = degree
+
+        if measure[0] == 'vector size':
+            rows[params]['vector size'] = result
+            continue
+
+        field = f'{measure[0]} ({unit})'
+        fields.add(field)
+        rows[params][field] = result
+
+    fields = ['scheme', 'degree', 'vector size'] + sorted(fields)
+    f = StringIO()
+    writer = csv.DictWriter(f, fieldnames=fields)
+    writer.writeheader()
+    writer.writerows(list(rows.values()))
+
+    print(f.getvalue())
+
+if __name__ == '__main__':
+    exit(main())
